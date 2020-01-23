eabc98ee03f52036c72752e5cff335f92f6ef2a8
Brian Nicholls <nichol88@gmail.com>
2020-01-23 22:22:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,5 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  def self.table_name
-    self.to_s.downcase.pluralize
-  end
 
-  def self.column_names
-
-  end
 end
