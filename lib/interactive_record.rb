4d1856498d1d70a69c13a60cfcbcf1a5bd281e24
Brian Nicholls <nichol88@gmail.com>
2020-01-23 22:24:06 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,11 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].execute("PRAGMA table_info(#{table_name})")
+  end
 end
