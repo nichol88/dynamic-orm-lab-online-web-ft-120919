cb8034c74aa585a0a82422c42ba319dac2c9c410
Brian Nicholls <nichol88@gmail.com>
2020-01-23 23:26:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,7 @@ class Student < InteractiveRecord
     values_array = []
 
     self.class.column_names.each do |col_name|
-      values_array << self.send(col_name)
+      values_array << "#{self.send(col_name)}"
     end
 
 
