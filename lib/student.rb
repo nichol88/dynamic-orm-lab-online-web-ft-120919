90504a8ba1cb5d495814c48bcc00da2f1aa7609b
Brian Nicholls <nichol88@gmail.com>
2020-01-24 00:10:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student < InteractiveRecord
     VALUES (#{values_for_insert})
     SQL
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT * from #{table_name_for_insert} get_last_insert_rowid")
+    @id = DB[:conn].execute("SELECT * from #{table_name_for_insert} get_last_insert_rowid")[0][0]
   end
 
 end
